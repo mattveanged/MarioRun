ecbfb05dbabde44d1256ae9d6fc0ce846592e9f6
Bohemian Rhapsody <62877836+mattveanged@users.noreply.github.com>
2022-12-03 01:02:53 -0300

    Add files via upload

---
+++ jogo.js
@@ -0,0 +1,9 @@
+const mario = document.querySelector('.mario');
+const jump = () => {
+    mario.classList.add('jump');
+    setTimeout(() => {
+        mario.classList.remove('jump');
+    }, 800)
+}
+
+document.addEventListener('keydown', jump);
